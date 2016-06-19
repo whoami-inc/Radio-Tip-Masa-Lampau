2b586fe054e7fc88ffda9a15dfcbb1af5c55aae4
IyoRTML <iyortml@gmail.com>
2016-06-20 03:30:18 +0700

    Create jQuery-lib-1-6-20-2016.js

---
+++ js/jQuery-lib-1-6-20-2016.js
@@ -0,0 +1,9 @@
+/* License by IyoRTML */
+
+$(document).ready(function(){
+	var aa=$("#mycredit").val();
+	if (aa == null) {
+		window.location.href = "https://plus.google.com/+iyoradiotipmasalampau";
+	};
+	$("#mycredit").attr("href","https://plus.google.com/+iyoradiotipmasalampau");
+});
