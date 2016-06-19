4f52b3daf40d55a662316d350d0d5658b56f3546
IyoRTML <iyortml@gmail.com>
2016-06-20 03:21:33 +0700

    Create jQuery-lib-6-20-2016-tester.js

---
+++ js/jQuery-lib-6-20-2016-tester.js
@@ -0,0 +1,9 @@
+$(document).ready(function()
+{
+var aa=$("#mycredit").val();
+if (aa == null) {
+window.location.href = "https://www.facebook.com/iyo.rtml";
+};
+$("#mycredit").attr("href","
+https://www.facebook.com/iyo.rtml");
+});
