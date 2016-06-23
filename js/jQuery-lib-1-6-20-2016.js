fb23115e44949c374604a4b38fd471b6db55a3ee
IyoRTML <iyortml@gmail.com>
2016-06-23 17:41:26 +0700

    Delete jQuery-lib-1-6-20-2016.js

--- js/jQuery-lib-1-6-20-2016.js
+++
@@ -1,9 +0,0 @@
-/* License by IyoRTML */
-
-$(document).ready(function(){
-	var aa=$("#mycredit").val();
-	if (aa == null) {
-		window.location.href = "https://plus.google.com/+iyoradiotipmasalampau";
-	};
-	$("#mycredit").attr("href","https://plus.google.com/+iyoradiotipmasalampau");
-});
