fcba79615124b1ad329b54f41709f084df2335f9
IyoRTML <iyortml@gmail.com>
2016-06-25 03:20:02 +0700

    Create jQuery-lib-iklan-6-25-2016.js

---
+++ Free/Ads/js/jQuery-lib-iklan-6-25-2016.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+	var aa=$("#iklan-rtml").val();
+	if (aa == null) {
+		window.location.href = "https://plus.google.com/+iyoradiotipmasalampau";
+	};
+	$("#iklan-rtml").attr("href","https://plus.google.com/+iyoradiotipmasalampau");
+});
+
+/* Ads Number 1 */
+$(document).ready(function(){
+	$("#adsby1").click(function(){
+		$("#iyoads1").slideToggle()
+	});
+});
+function closedUnit1() {
+	var r = confirm("Apakah iklan ini mengganggu? klik OK untuk menghapus iklan, atau klik Cancel dan membiarkan iklan ini tayang");
+	if (r == true) {
+		$("#iklanUnit1").hide()
+	}
+	else {
+	}
+}
+function adsUnit1() {
+	window.location.assign("http://radiotipmasalampau.blogspot.com");
+}
+/* End Ads Number 1 */
+
+/* Ads Number 2 */
+$(document).ready(function(){
+	$("#adsby2").click(function(){
+		$("#iyoads2").slideToggle()
+	});
+});
+function closedUnit2() {
+	var r = confirm("Apakah iklan ini mengganggu? klik OK untuk menghapus iklan, atau klik Cancel dan membiarkan iklan ini tayang");
+	if (r == true) {
+		$("#iklanUnit2").hide()
+	}
+	else {
+	}
+}
+function adsUnit2() {
+	window.location.assign("http://radiotipmasalampaurtml.blogspot.co.id/p/promo-iklan-gratis.html");
+}
+/* End Ads Number 2 */
+
+/* Ads Number 3 */
+$(document).ready(function(){
+	$("#adsby3").click(function(){
+		$("#iyoads3").slideToggle()
+	});
+});
+function closedUnit3() {
+	var r = confirm("Apakah iklan ini mengganggu? klik OK untuk menghapus iklan, atau klik Cancel dan membiarkan iklan ini tayang");
+	if (r == true) {
+		$("#iklanUnit3").hide()
+	}
+	else {
+	}
+}
+function adsUnit3() {
+	window.location.assign("http://radiotipmasalampaurtml.blogspot.co.id/p/promo-iklan-gratis.html");
+}
+/* End Ads Number 3 */
+
+/* Ads Number 4 */
+$(document).ready(function(){
+	$("#adsby4").click(function(){
+		$("#iyoads4").slideToggle()
+	});
+});
+function closedUnit4() {
+	var r = confirm("Apakah iklan ini mengganggu? klik OK untuk menghapus iklan, atau klik Cancel dan membiarkan iklan ini tayang");
+	if (r == true) {
+		$("#iklanUnit4").hide()
+	}
+	else {
+	}
+}
+function adsUnit4() {
+	window.location.assign("http://radiotipmasalampaurtml.blogspot.co.id/p/promo-iklan-gratis.html");
+}
+/* End Ads Number 4 */
+
+/* Ads Number 5 */
+$(document).ready(function(){
+	$("#adsby5").click(function(){
+		$("#iyoads5").slideToggle()
+	});
+});
+function closedUnit5() {
+	var r = confirm("Apakah iklan ini mengganggu? klik OK untuk menghapus iklan, atau klik Cancel dan membiarkan iklan ini tayang");
+	if (r == true) {
+		$("#iklanUnit5").hide()
+	}
+	else {
+	}
+}
+function adsUnit5() {
+	window.location.assign("http://radiotipmasalampaurtml.blogspot.co.id/p/promo-iklan-gratis.html");
+}
+/* End Ads Number 4 */
